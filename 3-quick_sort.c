9bec46c41e01ec7411e24cc5d76f96e1af97c90d
Abdulazizgr <abdulazizisa579@gmail.com>
2024-03-06 01:17:41 +0000

    sorting

---
+++ 3-quick_sort.c
@@ -0,0 +1,74 @@
+#include "sort.h"
+
+/**
+ * swap - Swaps two integers
+ * @a: Pointer to the first integer
+ * @b: Pointer to the second integer
+ */
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+/**
+ * lomuto_partition - Lomuto partition scheme
+ * @array: Array to be sorted
+ * @l: Starting index
+ * @h: Ending index
+ * @size: Size of the array
+ * Return: Partition index
+ */
+int lomuto_partition(int *array, int l, int h, size_t size)
+{
+    int piv = array[h];
+    int i = l - 1;
+    int j;
+
+    for (j = l; j <= h - 1; j++)
+    {
+        if (array[j] < piv)
+        {
+            i++;
+            swap(&array[i], &array[j]);
+            print_array(array, size);
+        }
+    }
+    swap(&array[i + 1], &array[h]);
+    print_array(array, size);
+    return (i + 1);
+}
+
+/**
+ * qck_sort - Quick sort function
+ * @array: Array to be sorted
+ * @l: Starting index
+ * @h: Ending index
+ * @size: Size of the array
+ */
+void qck_sort(int *array, int l, int h, size_t size)
+{
+    if (l < h)
+    {
+        int pi = lomuto_partition(array, l, h, size);
+
+        qck_sort(array, l, pi - 1, size);
+        qck_sort(array, pi + 1, h, size);
+    }
+}
+
+/**
+ * quick_sort - Sorts an array of integers with quick sort algorithm
+ * using the Quick sort algorithm
+ * @array: Array to be sorted
+ * @size: Size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+    if (array == NULL || size < 2)
+        return;
+
+    qck_sort(array, 0, size - 1, size);
+}
+
--- sort.h
+++ sort.h
@@ -24,6 +24,10 @@ void print_list(const listint_t *list);
 
 void selection_sort(int *array, size_t size);
 void bubble_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void qck_sort(int *array, int l, int h, size_t size);
+int lomuto_partition(int *array, int l, int h, size_t size);
+void swap(int *a, int *b);
 void insertion_sort_list(listint_t **list);
 listint_t *swap_node(listint_t *nd, listint_t **list);
 
